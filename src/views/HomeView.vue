a52d7922d972bb4337451809212a4d0da4bd30fc
Larissa <dejamepienso@gmail.com>
2024-03-27 12:47:58 -0600

    Add first config

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup>
+	import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+	<RouterView />
+</template>
+
+<style>
+	html {
+		scroll-behavior: smooth;
+	}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @format */
+/* eslint-env node */
+module.exports = {
+	content: [
+		// Example content paths...
+		"./public/**/*.html",
+		"./src/**/*.{js,jsx,ts,tsx,vue}",
+	],
+	theme: {
+		extend: {
+			
+		},
+		colors: {
+      black: "#010101",
+			blue: "#5075F7",
+			white: "#FFFFFF",
+		},
+	},
+	fontfamily: {
+		body: ["Mouse Memoirs, chivo"],
+	},
+
+	// ...
+};
