1715893d74e1d32da1c373db3312d5aebb8a26ba
Larissa <dejamepienso@gmail.com>
2024-04-12 15:40:10 -0600

    Componente dropdown

--- src/components/DropDown.vue
+++ src/components/DropDown.vue
@@ -1,6 +1,25 @@
+<!-- @format -->
+
 <script setup>
+	const modelValue = defineModel();
+	defineProps({
+		disabledValueText: {
+			type: String,
+		},
+	});
+
+	function updateModel() {
+		modelValue.value;
+	}
 </script>
 
 <template>
-  <h1>Aquí va el Select</h1>
+	<div class="custom-select custom-select custom-select::after">
+		<select
+			v-model="updateModel"
+			class="w-[54px] border border-b-1 border-t-0 border-l-0 border-r-0 text-[16px] text-blue indent-[4px] text-center">
+			<option disabled value="">{{ disabledValueText }}></option>
+			<option v-for="i in 31" :key="i" :value="i">{{ i }}</option>
+		</select>
+	</div>
 </template>
